8649d3c80f8172c29acf94db4f0e74af26981250
tavolafourcade <tavolafourcade@gmail.com>
2021-08-04 00:51:11 +0000

    Fixed indentation

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,26 +1,28 @@
 #!/usr/bin/node
 // Class for rectangle with height and width parameters and various methods
 
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (!(w <= 0 || h <= 0) && w && h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-module.exports = class Rectangle{
-	constructor(w,h){
-		if(w>0 && h>0){
-			this.width = w;
-			this.height = h;
-		}
-	}
-	print(){
-		for (let i = 0; i < this.height; i++){
-			console.log('x'.repeat(this.width));
-		}
-	}
-	rotate(){
-		let temp = this.width;
-		this.width = this.height;
-		this.height = temp;
-	}
-	double(){
-		this.width *= 2;
-		this.height *=2;
-	}
-}
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
