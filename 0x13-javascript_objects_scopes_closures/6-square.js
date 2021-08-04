c6f9bc1b2eb93514a2f97d3f20d0d3d95243d898
tavolafourcade <tavolafourcade@gmail.com>
2021-08-04 00:54:37 +0000

    Fixed indentation

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 // Square class that inherits from the Rectangle class
 
-const Square_ = require('./5-square');
+const SquarePar = require('./5-square.js');
 
-module.exports = class Square extends Square_{
-	charPrint(c){
-		if(!c){
-			c = 'x';
-		}
-		
-		for (let i =0; i < this.width; i++){
-			console.log(c.repeat(this.width));
-		}
-	}
-}
+module.exports = class Square extends SquarePar {
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
