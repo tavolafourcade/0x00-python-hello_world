55f4fb66cec854b26023e5ce7d8950af487f616a
tavolafourcade <tavolafourcade@gmail.com>
2021-08-04 00:52:25 +0000

    Fixed indentation

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,8 +3,8 @@
 
 const Rectangle = require('./4-rectangle');
 
-module.exports = class Square extends Rectangle{
-	constructor(size){
-		super(size,size)
-	}
-}
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
