ff1b54406c8ecb7346bcfa91aef718efdb8f8af1
tavolafourcade <tavolafourcade@gmail.com>
2021-08-04 00:42:14 +0000

    Fixed bug

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,9 +2,9 @@
 // Class for rectangle with a constructor that initializes two instance
 // attributes
 
-module.exports = class Rectangle{
+module.exports = class Rectangle {
 	constructor (w, h){
 		this.width = w;
 		this.height = h;
 	}
-}
+};
