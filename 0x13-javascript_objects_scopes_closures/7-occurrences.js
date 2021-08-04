cfc14d2c9b7a909882c70a8d93fd95ad9d8e9f16
tavolafourcade <tavolafourcade@gmail.com>
2021-08-04 00:56:20 +0000

    Fixed indentation

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 // Returns the number of occurrences in a list
-exports.nbOccurences = function (list, searchElement){
-	let count = 0;
-	for (let i = 0; i < list.length; i++){
-		if(list[i]===searchElement){
-			count++;
-		}
-	}
-	return count;
-}
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count += 1;
+    }
+  }
+  return (count);
+};
