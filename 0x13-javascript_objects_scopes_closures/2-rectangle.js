0709858ffefc25d6c481a7d1f664a9ced1a739d7
tavolafourcade <tavolafourcade@gmail.com>
2021-08-04 00:48:24 +0000

    Implement a new version

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,8 +3,8 @@
 // attributes
 
 module.exports = class Rectangle {
-  constructor(w, h){
-    if (w > 0 && h > 0){
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
